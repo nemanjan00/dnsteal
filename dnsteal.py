486635920eeff6337c11dfe26d949f1c52de4381
nemanjan00 <nemanjan00@gmail.com>
2015-09-28 12:52:13 +0200

    Bug fix

--- dnsteal.py
+++ dnsteal.py
@@ -83,10 +83,11 @@ if __name__ == '__main__':
     data = p.data_text.split(".")
 
     for index,item in enumerate(data):
-      if(index == 1):
-        filename = item
-      if(index > 1):
-        filename += "." + item
+      if(index < len(data) - 2):
+        if(index == 1):
+          filename = item
+        if(index > 1):
+          filename += "." + item
 
     filename = filename[:-1]
 
